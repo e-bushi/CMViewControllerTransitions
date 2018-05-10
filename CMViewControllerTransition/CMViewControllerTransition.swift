71bdc42092686019004843b11b61b4e4aea75ccb
Chris Mauldin <Chrislovemauldin@gmail.com>
2018-05-10 14:24:21 -0700

    Initial commit for custom view controller transition library

---
+++ CMViewControllerTransition/CMViewControllerTransition.swift
@@ -0,0 +1,11 @@
+//
+//  CMViewControllerTransition.swift
+//  CMViewControllerTransition
+//
+//  Created by Chris Mauldin on 5/10/18.
+//  Copyright © 2018 Chris Mauldin. All rights reserved.
+//
+
+class CMViewControllerTransition {
+
+}
